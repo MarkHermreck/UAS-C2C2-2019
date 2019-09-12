3336a11edced3598d6c309ea9a96a57275f37cbf
Mark Hermreck <markhermreck@gmail.com>
2019-09-12 14:36:11 -0500

    Update navigation.py

--- drone/navigation.py
+++ drone/navigation.py
@@ -8,10 +8,9 @@ from dronekit import connect, LocationGlobal, LocationGlobalRelative, Vehicle, V
 from pymavlink import mavutil
 import time, math, dronekit_sitl
 
-
-#This block of code connects to the UAV over its serial connection, Pi -> PixHawk
-#It also initalizes the vehicle object referenced throughout this file.
-#When the connection information is known, fill out the portInformation variable to disable auto-start of SITL.
+# This block of code connects to the UAV over its serial connection, Pi -> PixHawk
+# It also initalizes the vehicle object referenced throughout this file.
+# When the connection information is known, fill out the portInformation variable to disable auto-start of SITL.
 portInformation = None
 testSITL = None
 
@@ -23,19 +22,18 @@ if not portInformation:
 print("Connecting to UAV.")
 UAV = connect(portInformation, wait_ready=True)
 
-#wait for radio transmission from ground station UI, store transmitted values into this array
-#0-1 are ISU 1 lat/long, 2-3 ISU 2 lat/long, 4-5 gnd station lat/long
+# wait for radio transmission from ground station UI, store transmitted values into this array
+# 0-1 are ISU 1 lat/long, 2-3 ISU 2 lat/long, 4-5 gnd station lat/long
 GPSCoordinates = [];
-
-
 
 '''
 This function arms the UAV and initializes its takeoff to an altitude determined by the goalAltitude variable.
 The only input is the takeoff goal altitude, but returns nothing.
 Input Variables: goalAltitude in meters, integer.
 '''
-def takeoffSequence(goalAltitude):
 
+
+def takeoffSequence(goalAltitude):
     print("Takeoff Sequence Initializing. - Ensure area above UAV is clear.")
 
     while not UAV.is_armable:
@@ -53,12 +51,12 @@ def takeoffSequence(goalAltitude):
     print("Initiating takeoff sequence.")
     UAV.simple_takeoff(goalAltitude)
 
-
-    while not UAV.location.global_relative_frame.alt >= goalAltitude *.90:
+    while not UAV.location.global_relative_frame.alt >= goalAltitude * .90:
         print("Vehicle taking off. Current altitude: ", UAV.location.global_relative_frame.alt)
         time.sleep(1)
 
     print("Takeoff complete.")
+
 
 '''
 This function takes two LocationGlobalRelative objects and returns the distance between them in meters.
@@ -66,12 +64,13 @@ It is based on the ArduPilot file using it at https://github.com/ArduPilot/ardup
 It is also found in the DroneKit documentation at https://dronekit-python.readthedocs.io/en/stable/
 Input Variables: Location1, LocationGlobalRelative. Location2, LocationGlobalRelative. 
 '''
-def distanceRelative(Location1, Location2):
 
+
+def distanceRelative(Location1, Location2):
     latitude = Location2.lat - Location2.lat
     longitude = Location2.lon - Location2.lon
 
-    distMeters = math.sqrt((latitude * latitude)+ (longitude * longitude)) * 1.113195e5
+    distMeters = math.sqrt((latitude * latitude) + (longitude * longitude)) * 1.113195e5
 
     return distMeters
 
@@ -82,11 +81,12 @@ LocationGlobalRelative object offset from the original location according to the
 It is based on the DroneKit documentation at https://dronekit-python.readthedocs.io/en/stable/
 Input Variables: ISU, LocationGlobalRelative. offsetNorth, offsetEast, integers in meters.
 '''
-def searchLocation(ISU, offsetNorth, offsetEast):
 
+
+def searchLocation(ISU, offsetNorth, offsetEast):
     dimensionRad = 6378137.0
-    offsetLat = offsetNorth/dimensionRad
-    offsetLong = offsetEast/ (dimensionRad * math.cos(math.pi * ISU.lat / 180))
+    offsetLat = offsetNorth / dimensionRad
+    offsetLong = offsetEast / (dimensionRad * math.cos(math.pi * ISU.lat / 180))
 
     newLat = ISU.lat + (offsetLat * 180 / math.pi)
     newLong = ISU.long + (offsetLong * 180 / math.pi)
@@ -101,8 +101,9 @@ This function is the basic travel-to-point function that will be utilized to tra
 to the ground station in turn.
 Input Variables: latitude, longitude, floats.
 '''
-def travel(latitude, longitude, alt):
 
+
+def travel(latitude, longitude, alt):
     print("Initializing travel to coordinates: ", latitude, ", ", longitude)
     if not UAV.location.global_relative_frame.alt >= 10:
         print("UAV altitude under 10 meters, aborting travel.")
@@ -122,8 +123,8 @@ def travel(latitude, longitude, alt):
     print("Waypoint reached. Commencing hover.")
     return None
 
-#still workin
 
+# still workin
 
 
 '''
@@ -134,13 +135,14 @@ Each next four increases the diagonal distance from original ISU location by 50 
 Input Variables: searchPoints, integer. ISULocation, LocationGlobalRelative.
 Returns: 1, indicating successful location of ISU. 0, indicating failure to locate ISU. None, function/input error.
 '''
-def searchPattern(searchPoints, ISULocation):
 
-    #35.355 meters offset to N/E for 50 meter right triangle hypotenuse
+
+def searchPattern(searchPoints, ISULocation):
+    # 35.355 meters offset to N/E for 50 meter right triangle hypotenuse
 
     print("Search pattern initializing with ", searchPoints, " discrete points.")
 
-    #First, checking to make sure the UAV is in the air, armed, etc.
+    # First, checking to make sure the UAV is in the air, armed, etc.
     if not UAV.location.global_relative_frame.alt >= 10:
         print("UAV under 10 meters, aborting search pattern.")
         return None
@@ -148,45 +150,43 @@ def searchPattern(searchPoints, ISULocation):
     if UAV.system_status == ACTIVE:
         print("UAV active, beginning search pattern.")
 
-
-    #Sets searchPoints to the next highest multiple of 4 if it not already set to one.
-    if searchPoints % 4 != 0 :
+    # Sets searchPoints to the next highest multiple of 4 if it not already set to one.
+    if searchPoints % 4 != 0:
         if searchPoints == 0:
             print("Called function with zero search points, aborting.")
             return None;
-        searchPoints = searchPoints + (4-(searchPoints % 4));
+        searchPoints = searchPoints + (4 - (searchPoints % 4));
         print("Overruling user input, scaling to multiple of 4. Final value: ", searchPoints)
 
-    #generating searchPoints number of locations to ping ISU at
-    #these locations are the corners of concentric squares
+    # generating searchPoints number of locations to ping ISU at
+    # these locations are the corners of concentric squares
 
     pingLocs = []
-    squares = int(searchPoints/4)
+    squares = int(searchPoints / 4)
 
     for x in range(searchPoints):
         corner = x % 4;
         if corner == 0:
-            pingLocs[x] = searchLocation(ISULocation, 35.355*squares, 35.355*squares)
+            pingLocs[x] = searchLocation(ISULocation, 35.355 * squares, 35.355 * squares)
         if corner == 1:
-            pingLocs[x] = searchLocation(ISULocation, -35.355*squares, 35.355*squares)
+            pingLocs[x] = searchLocation(ISULocation, -35.355 * squares, 35.355 * squares)
         if corner == 2:
-            pingLocs[x] = searchLocation(ISULocation, -35.355*squares, -35.355*squares)
+            pingLocs[x] = searchLocation(ISULocation, -35.355 * squares, -35.355 * squares)
         if corner == 3:
-            pingLocs[x] = searchLocation(ISULocation, 35.355*squares, -35.355*squares)
+            pingLocs[x] = searchLocation(ISULocation, 35.355 * squares, -35.355 * squares)
 
-    #this loop tells the drone to go to each of the locations created above, and ping the ISU
-    #exits function upon successful ping, because data can be transferred
-    #if it reaches the end of the list with no successful ping, returns 0
+    # this loop tells the drone to go to each of the locations created above, and ping the ISU
+    # exits function upon successful ping, because data can be transferred
+    # if it reaches the end of the list with no successful ping, returns 0
 
     for y in range(searchPoints):
 
         travel(pingLocs[0].lat, pingLocs[y].lon, 30)
-        #ping drone, return 1 or 0 if success/fail
+        # ping drone, return 1 or 0 if success/fail
         placeholder = 3
         if placeholder == 1:
             print("ISU communication established, exiting search pattern successfully.")
             return 1;
-
 
     print("Search pattern executed, no ISU located. Exiting.")
     return 0;
@@ -200,8 +200,8 @@ Input Variables: homeLat, homeLong, floats.
 '''
 def landingSequence(homeLat, homeLong):
 
+    print("Initiating landing sequence, standby to catch drone.")
+    homeLocation = LocationGlobalRelative(homeLat,homeLong,0);
 
 
-    if not distanceRelative()
-
-
+    if not distanceRelative() < 10
