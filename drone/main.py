952b75d2bc435b0cf800f23fdcecf1c7131e9629
Mark Hermreck <markhermreck@gmail.com>
2019-10-07 17:14:06 -0500

    Update main.py

--- drone/main.py
+++ drone/main.py
@@ -5,9 +5,9 @@ Questions about this code can be directed toward Mark Hermreck at markhermreck@g
 """
 
 #importing all the functions and sundries needed from dronekit, mavlink, and the navigation module
-from dronekit import connect, LocationGlobal, LocationGlobalRelative, Vehicle, VehicleMode, Command
-from pymavlink import mavutil
+from dronekit import connect, LocationGlobal, LocationGlobalRelative, Vehicle, VehicleMode
 from navigation import takeoffSequence, distanceRelative, searchLocation, travel, searchPattern, landingSequence
+from navigation import safetyChecks
 import time, math, dronekit_sitl
 
 # This block of code connects to the UAV over its serial connection, Pi -> PixHawk
@@ -28,17 +28,16 @@ UAV = connect(portInformation, wait_ready=True)
 
 # wait for radio transmission from ground station UI, store transmitted values into this array
 # 0-1 are ISU 1 lat/long, 2-3 ISU 2 lat/long, 4-5 gnd station lat/long
-GPSCoordinates = [-35.364,149.167, 0, 0, 0, 0];
+GPSCoordinates = [-35.364,149.167, -35.365, 149.168, 0, 0];
 
 #default location is canberra, australia
 #-35.363261
 #149.1652299
 
-
 print(UAV.location.global_relative_frame.lat)
 print(UAV.location.global_relative_frame.lon)
 
-#declares three LGR objects in case they're needed. Default alti
+#declares three LGR objects in case they're needed. Default altitude is also declared here; for now it's 30m
 alt = 30;
 ISU1Location = LocationGlobalRelative(GPSCoordinates[0], GPSCoordinates[1], alt)
 ISU2Location = LocationGlobalRelative(GPSCoordinates[2], GPSCoordinates[3], alt)
@@ -53,9 +52,9 @@ travel(GPSCoordinates[0],GPSCoordinates[1], alt, UAV)
 UAV.mode = VehicleMode("LOITER")
 ISUOne = 0; #com.ping()
 if not ISUOne:
-    UAV.mode = VehicleMode("GUIDED")
+    safetyChecks(UAV, homeLoc)
     searchPattern(3, UAV.location.global_relative_frame, UAV)
-UAV.mode = VehicleMode("GUIDED")
+safetyChecks(UAV, homeLoc)
 
 
 #travels to second ISU location,
@@ -63,9 +62,9 @@ travel(GPSCoordinates[2],GPSCoordinates[3], alt, UAV)
 UAV.mode = VehicleMode("LOITER")
 ISUTwo = 0; #com.ping()
 if not ISUTwo:
-    UAV.mode = VehicleMode("GUIDED")
+    safetyChecks(UAV, homeLoc)
     searchPattern(3, UAV.location.global_relative_frame, UAV)
-UAV.mode = VehicleMode("GUIDED")
+safetyChecks(UAV, homeLoc)
 
 #returns home
 landingSequence(homeLoc,UAV)
