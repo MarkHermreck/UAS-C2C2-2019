82a85ca5c796c8d327828f9c200d3f28dc723d13
Mark Hermreck <markhermreck@gmail.com>
2019-10-16 19:46:49 -0500

    Update main.py

--- Sensors/main.py
+++ Sensors/main.py
@@ -1,17 +1,44 @@
-import os, sys
-from temp_sensors import temp_sensors
-from motion_det import motion_det
+"""
+Property of UAS C2C2 Senior Design Team 2019
+Team Members: Mark Hermreck, Joseph Lisac, Khaled Alshammari, Khaled Alharbi
+Questions about this code can be directed toward Mark Hermreck at markhermreck@gmail.com
+"""
 
-tempThresh = 40
-humThresh = 10
+import time, logging, sys, temperusb, temper
+from communication import Communication
+
+# Connect to xBee
+COM_CONNECTION_STRING = '/dev/ttyUSB1'      #potential option
+#com = Communication(COM_CONNECTION_STRING, 0.1)
+
+#three things need to be done
+#get temperature every X
+#monitor
+
+#creating data file
+logFile = open("ISULogs.txt", "a+")
+logFile.truncate(0);
+logTime = time.time();
+
+
+
+"""
+This function is called every 5 minutes and is responsible for pulling temperature data and appending it to the end
+of the file created above in main. This will be placed in the temperature handler file, whenever that's done. Will also
+need to adjust this function to have temperature variable be the value returned by the usb
+Inputs: logFile, the txt file created earlier in the main function. 
+"""
+def logTemperature(logfile):
+    timeString = str(time.ctime(time.time()))
+    temperature = 0;
+    #temperature = temperatureHandler.get_temperatures();
+    logfile.write("Temperature data recorded @ " + timeString + " with a value of " + str(temperature) + " degrees Celsius")
+    return None;
+
 
 while True:
-    Temp,Humidity = temp_sensors.temp_data()
-    Motion = motion_det.motion_data()
 
-    C = Temp
-    H = Humidity
+    if time.time() - logTime >= 300:
+        logTemperature(logFile)
+        logTime = time.time()
 
-    if C!=-1000 or H!=-1000:
-        if C>tempThresh or H>humThresh or Motion==True:
-            os.system('say "Motion Detected"')
