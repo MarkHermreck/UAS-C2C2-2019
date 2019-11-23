5c96529a2797f9cccb6aaa0d2c7ccf5ed684b31c
jtlisac <48691572+jtlisac@users.noreply.github.com>
2019-11-23 13:27:42 -0600

    temperature sensor code

---
+++ Sensors/temp_sensor_DHT22.py
@@ -0,0 +1,12 @@
+import Adafruit_DHT
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+while True:
+    _, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+    if temperature is not None:
+        print("Temp={0:0.1f}*C".format(temperature))
+    else:
+        print("Failed to retrieve data from humidity sensor")
